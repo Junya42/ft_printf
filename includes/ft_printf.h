b3f45dc1f3f33ff49f6d9e705026b1604bd8e706
Junya <remiki.anis3@gmail.com>
2022-04-03 16:51:21 +0200

    .h update

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: anremiki <anremiki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/17 00:37:35 by anremiki          #+#    #+#             */
-/*   Updated: 2021/10/17 05:17:33 by anremiki         ###   ########.fr       */
+/*   Updated: 2022/04/03 16:50:53 by anremiki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,12 @@
 # include <stdlib.h>
 # include <stdarg.h>
 
-int	ft_putchar(char c);
-int	ft_putstr(char *str);
-int	ft_putnbrsize(int n);
-int	ft_putunbrsize(unsigned int n);
-int	ft_returnputx(unsigned int n, char x);
-int	ft_returnaddress(unsigned long int n, char x);
+int	ft_putchar(char c, int fd);
+int	ft_putstr(char *str, int fd);
+int	ft_putnbrsize(int n, int fd);
+int	ft_putunbrsize(unsigned int n, int fd);
+int	ft_returnputx(unsigned int n, char x, int fd);
+int	ft_returnaddress(unsigned long int n, char x, int fd);
 int	ft_printf(const char *format, ...);
 
 #endif
